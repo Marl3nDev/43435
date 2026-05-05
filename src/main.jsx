392110100df0f1ac1e89208607fc01d6eca8a6c6
Marlen <managadzem5@gmail.com>
2026-05-05 17:04:50 +0400

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { App } from '@/app/App.jsx'
 import { QueryProvider } from '@/app/providers/QueryProvider/QueryProvider.jsx';
@@ -8,9 +8,9 @@ import { ReduxProvider } from '@/app/providers/ReduxProvider/ReduxProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <ReduxProvider>
     <QueryProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App/>
-      </HashRouter>
+      </BrowserRouter>
     </QueryProvider>
   </ReduxProvider>
 )
